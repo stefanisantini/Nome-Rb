864d1f77b2c412afa76131dfced77b2bd25b1a94
Stéfani Santini Reis <110628639+stefanisantini@users.noreply.github.com>
2023-03-06 19:31:52 -0300

    Add files via upload
    Adição do nome e sobrenome através da linguagem Ruby

---
+++ hello.rb
@@ -0,0 +1,13 @@
+# Saida de Dado
+print 'Digite seu nome: '
+
+# Recebendo uma entrada no teclado
+name = gets.chomp
+
+print 'Digite seu sobrenome: '
+
+sobrenome = gets.chomp
+
+# saida utilizando puts
+# use codigo ruby  dentro de uma string com #{code}
+puts "Hello #{name} #{sobrenome}!"
